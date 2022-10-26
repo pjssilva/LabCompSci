e43b9c820182d9374bad1cc0e6aac6d05bb11d10
Paulo J. S. Silva <pjssilva@gmail.com>
2022-10-26 20:15:51 -0300

    Ajustes para aula

--- cadernos/semana09/18_otimizacao.jl
+++ cadernos/semana09/18_otimizacao.jl
@@ -7,7 +7,11 @@ using InteractiveUtils
 # This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
 macro bind(def, element)
     quote
-        local iv = try Base.loaded_modules[Base.PkgId(Base.UUID("6e696c72-6542-2067-7265-42206c756150"), "AbstractPlutoDingetjes")].Bonds.initial_value catch; b -> missing; end
+        local iv = try
+            Base.loaded_modules[Base.PkgId(Base.UUID("6e696c72-6542-2067-7265-42206c756150"), "AbstractPlutoDingetjes")].Bonds.initial_value
+        catch
+            b -> missing
+        end
         local el = $(esc(element))
         global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : iv(el)
         el
@@ -17,13 +21,13 @@ end
 # ╔═╡ 400ebe26-0dea-4cf2-8744-6c73a45cd33e
 begin
     using PlutoUI
-	using Plots
-	using Statistics
-	using Optim
-	using JuMP
-	using Ipopt
-	using ForwardDiff
-	using BenchmarkTools
+    using Plots
+    using Statistics
+    using Optim
+    using JuMP
+    using Ipopt
+    using ForwardDiff
+    using BenchmarkTools
 end
 
 # ╔═╡ b8d66df5-f593-40b4-8c46-3b638f9cc3e1
@@ -58,17 +62,17 @@ n = $(@bind n Slider(3:10:200, show_value=true, default=193))
 """
 
 # ╔═╡ f8c98995-2152-4d45-996a-a0532a233719
-x = sort((rand( -10:100, n)))
+x = sort((rand(-10:100, n)))
 
 # ╔═╡ 8a5f1fdc-3cef-4c02-a73f-e5975b57b15a
-y = 5/9 .* x .- 17.7777777  .+  5 .* randn.() #  same as y =  5/9 .* (x .- 32)
+y = 5 / 9 .* x .- 17.7777777 .+ 5 .* randn.() #  same as y =  5/9 .* (x .- 32)
 
 # ╔═╡ 647093eb-a7e3-4175-8091-29c33407e5c9
 begin
-	plot(x,y, m=:c, mc=:red, legend=false, ls=:dash)
-	xlabel!("°F")
-	ylabel!("°C")
-	# plot!( x, (x.-30)./2)  # Dave's cool approximation
+    plot(x, y, m=:c, mc=:red, legend=false, ls=:dash)
+    xlabel!("°F")
+    ylabel!("°C")
+    # plot!( x, (x.-30)./2)  # Dave's cool approximation
 end
 
 # ╔═╡ cdc25782-65a8-43c5-8090-c1241b798b1a
@@ -97,13 +101,13 @@ md"""
 
 # ╔═╡ da0d208b-7d30-470a-b180-4cbfa98298e7
 begin
-	m = cov(x, y) / var(x) # same as (x.-mean(x))⋅(y.-mean(y))/sum(abs2,x.-mean(x))
-	b = mean(y) - m * mean(x)
-	right_coefs = (b=b, m=m)
+    m = cov(x, y) / var(x) # same as (x.-mean(x))⋅(y.-mean(y))/sum(abs2,x.-mean(x))
+    b = mean(y) - m * mean(x)
+    right_coefs = (b=b, m=m)
 end
 
 # ╔═╡ 5503b4de-0b53-4223-8ce0-5e014be3f7ab
-plot!(x -> m*x + b, lw=3, alpha=0.7)
+plot!(x -> m * x + b, lw=3, alpha=0.7)
 
 # ╔═╡ 05e512ca-3123-48d9-9c11-5d6e9d90ef95
 md"""
@@ -156,10 +160,10 @@ ou
 """
 
 # ╔═╡ 372b304a-3f57-4bec-88df-3d51ded57d5c
-loss( (b, m) ) = sum( (b + m*x[i] - y[i])^2  for i=1:n )
+loss((b, m)) = sum((b + m * x[i] - y[i])^2 for i = 1:n)
 
 # ╔═╡ 13b9ff38-225d-4ec1-be7f-bf0e0f5b4076
-result =  optimize(loss, [0.0,0.0] )  # optimize f with starting guess
+result = optimize(loss, [0.0, 0.0])  # optimize f with starting guess
 
 # ╔═╡ 7bd9bb8f-36c5-4ae1-ba20-25732d7fef2e
 result.minimizer
@@ -184,18 +188,18 @@ JuMP = Julia for Mathematical Programming
 
 # ╔═╡ b7d8f11d-91ce-4b3a-87a1-1aa162e198ff
 let
-	n = length(x)
-	model = Model(Ipopt.Optimizer)
+    n = length(x)
+    model = Model(Ipopt.Optimizer)
 
-	@variable(model, b)
-	@variable(model, m)
+    @variable(model, b)
+    @variable(model, m)
 
-    @objective(model, Min, sum( (b + m*x[i] - y[i])^2 for i in 1:n) )
+    @objective(model, Min, sum((b + m * x[i] - y[i])^2 for i in 1:n))
 
-	# set_silent(model)
-	optimize!(model)
+    # set_silent(model)
+    optimize!(model)
 
-	(b=value(b), m=value(m))
+    (b=value(b), m=value(m))
 end
 
 # ╔═╡ 5ca85768-a19e-4ddf-89a4-88dca599d7a7
@@ -227,8 +231,8 @@ md"""
 
 # ╔═╡ 6f64ede7-612e-47b3-b3a4-d22a1992a98d
 begin
-	∇loss(b, m, i) = 2*(b+m*x[i]-y[i]) .* [1,x[i]] # ith summand
-	∇loss(b, m) = sum(∇loss(b, m, i) for i=1:n)
+    ∇loss(b, m, i) = 2 * (b + m * x[i] - y[i]) .* [1, x[i]] # ith summand
+    ∇loss(b, m) = sum(∇loss(b, m, i) for i = 1:n)
 end
 
 # ╔═╡ 36300b71-5a96-4964-b661-93de5631cf07
@@ -237,12 +241,12 @@ md"""
 """
 
 # ╔═╡ ad578b33-4387-49f5-b39d-92e05fca4ea5
-∇loss(.1, .3)
+∇loss(0.1, 0.3)
 
 # ╔═╡ 67fd90d7-bb34-411f-89f1-a410e6fb29ba
 begin # finite difference
-	ϵ = 1.0e-8
-	([loss([.1+ϵ ,.3]);loss([.1 ,.3+ϵ])] .- loss([.1 ,.3])) ./ ϵ
+    ϵ = 1.0e-8
+    ([loss([0.1 + ϵ, 0.3]); loss([0.1, 0.3 + ϵ])] .- loss([0.1, 0.3])) ./ ϵ
 end
 
 # ╔═╡ 3e229e4a-a697-460e-b995-a4773a6aca70
@@ -253,10 +257,10 @@ Não importa se somos bons ou não em cálculo. O processo manual não escala fa
 """
 
 # ╔═╡ 7566cb7e-f5da-4b81-af07-bf2c86963333
-∇loss(.1,.3) # hand computation
+∇loss(0.1, 0.3) # hand computation
 
 # ╔═╡ e6d9aafd-fbd6-4ec4-a4a1-740a4e889dc5
-ForwardDiff.gradient(loss, [.1,.3])
+ForwardDiff.gradient(loss, [0.1, 0.3])
 
 # ╔═╡ c9417d90-a9cb-4655-a258-8a8898e5576a
 md"""
@@ -271,18 +275,18 @@ Hoje há um ressurgimento de métodos de primeira ordem porque problemas em Ciê
 
 # ╔═╡ 6535280a-e0ce-4e13-86dd-165d5f06cfe7
 let
-	b,m = 0,0  # starting guess
+    b, m = 0, 0  # starting guess
 
-	for i=1:25
-		db,dm = ∇loss(b,m)
+    for i = 1:25
+        db, dm = ∇loss(b, m)
 
-		# Getting a good step size can be really hard
-		# I worked out the line search η by hand
-		η = sum( (b+m*x[i]-y[i])*(db+dm*x[i]) for i=1:n)/sum( (db+dm*x[i])^2 for i=1:n)
+        # Getting a good step size can be really hard
+        # I worked out the line search η by hand
+        η = sum((b + m * x[i] - y[i]) * (db + dm * x[i]) for i = 1:n) / sum((db + dm * x[i])^2 for i = 1:n)
 
-		b,m  = (b,m) .- η .* (db,dm)
-	end
-	(b=b,m=m)
+        b, m = (b, m) .- η .* (db, dm)
+    end
+    (b=b, m=m)
 end
 
 # ╔═╡ 4c285bc2-b3c2-4d20-a904-ecaa07795342
@@ -301,14 +305,14 @@ Pegue um dos termos da soma (ou alguns deles) para atualizar de acordo com o res
 
 # ╔═╡ 7086950b-c8db-49d4-b095-15be91c73b56
 let
-	b, m  = 0.0, 0.0
-	for t=1:10_000_000
-	    η = .00002  # there seems to be an art to picking these steplengths
-	 
-	    b,m  =  (b,m) .- η *∇loss(b, m, rand(1:n))
-	   
-	end
-   	(b=b,m=m)
+    b, m = 0.0, 0.0
+    for t = 1:10_000_000
+        η = 0.00002  # there seems to be an art to picking these steplengths
+
+        b, m = (b, m) .- η * ∇loss(b, m, rand(1:n))
+
+    end
+    (b=b, m=m)
 end
 
 # ╔═╡ 327514f1-8081-4a6c-8be4-8ffd52ed3c46
@@ -317,16 +321,16 @@ md"""
 """
 
 # ╔═╡ 98e00b2d-0802-4160-8e5c-302be5226916
-optimize(loss, [0.0,0.0], BFGS())
+optimize(loss, [0.0, 0.0], BFGS())
 
 # ╔═╡ ef165ca5-bf4f-465e-8e9a-df1aec2d7caa
-optimize(loss, [0.0,0.0], BFGS(), autodiff=:forward)
+optimize(loss, [0.0, 0.0], BFGS(), autodiff=:forward)
 
 # ╔═╡ 0305b418-51bb-47bb-98fb-319fc26b94cf
-optimize(loss, [0.0,0.0], GradientDescent() )
+optimize(loss, [0.0, 0.0], GradientDescent())
 
 # ╔═╡ 304a3a6e-c8c3-48d8-a101-313b3aa062f2
-optimize(loss, [0.0,0.0], GradientDescent(), autodiff=:forward )
+optimize(loss, [0.0, 0.0], GradientDescent(), autodiff=:forward)
 
 # ╔═╡ 00000000-0000-0000-0000-000000000001
 PLUTO_PROJECT_TOML_CONTENTS = """
